05ed5cca18a2bf52f8c11d7a445c9c406817c338
Ghhhhhhhj <pr.polis.p@gmail.com>
2024-08-08 19:59:20 +0200

    added name to hello

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-def goodbye(f):
-    def wrapper():
-        f()
+def goodbye(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
         print("Goodbye!")
     return wrapper
 
 @goodbye
-def hello():
-    print("Hello, world!")
+def hello(name):
+    print(f"Hello, {name}")
 
-hello()
+hello("David")
