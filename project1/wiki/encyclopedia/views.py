cf2f8d76fb8422b85750c2229b7fbfe407b491bb
Ghhhhhhhj <pr.polis.p@gmail.com>
2024-08-19 23:41:41 +0200

    Finished Entry Page part

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
-
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, entry):
+    markdowner = Markdown()
+    if util.get_entry(entry):
+        return render(request, "encyclopedia/entry.html", {
+            "entry": markdowner.convert(util.get_entry(entry)),
+            "title": entry
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": "This entry does not exist."
+        })
 
