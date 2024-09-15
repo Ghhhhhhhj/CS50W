c9c79ac41e100be2081c9ec996959abf7c54e889
Ghhhhhhhj <pr.polis.p@gmail.com>
2024-09-15 22:05:53 +0200

    Finished Send Mail and Mailbox

--- project3/mail/mail/static/mail/inbox.js
+++ project3/mail/mail/static/mail/inbox.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +32,77 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  load_emails(mailbox);
+}
+
+function send_email(event) {
+  event.preventDefault();
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      console.log(result);
+      load_mailbox('sent')
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+  ;
+}
+
+function load_emails(mailbox) {
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+
+      document.querySelector('#emails-view').innerHTML = '';
+      console.log(emails);
+
+      emails.sort((a, b) => {
+        return new Date(b.timestamp) - new Date(a.timestamp);
+      });
+      emails.forEach(email => {
+
+        
+
+        const isRead = email.read;
+        const isArchived = email.archived;
+
+        if(isArchived){
+          return;
+        }
+
+        const element = document.createElement('div');
+        element.classList.add('email-item');
+
+        const sender = document.createElement('strong');
+        sender.textContent = email.sender;
+
+        const subject = document.createElement('span')
+        subject.textContent = email.subject
+
+        const timestamp = document.createElement('span')
+        timestamp.textContent = email.timestamp;
+        timestamp.classList.add('timestamp');
+
+        if(isRead) {
+          element.style.backgroundColor='lightgray'
+        }
+
+        element.appendChild(sender);
+        element.appendChild(subject);
+        element.appendChild(timestamp);
+
+        document.querySelector('#emails-view').appendChild(element)
+      });
+      
+
+  });
+
 }
